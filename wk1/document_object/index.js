fc5e40dd4d031099dbe129c1cd5df305c1ceeb7b
caidoole <caitlin.dooley@bryanuniversity.edu>
2020-04-11 14:14:28 -0700

    staging week one

---
+++ wk1/document_object/index.js
@@ -0,0 +1,5 @@
+document
+
+
+console.log(document)
+
---
+++ wk1/selecting_elements/index.js
@@ -0,0 +1,14 @@
+var header = document.getElementById("testing");
+header.textContent += "This is text";
+
+console.log (testing)
+console.dir (testing)
+
+
+var heading = document.getElementById("secondTest");
+var text = document.createTextNode("This is Text");
+
+heading.appendChild(text);
+
+console.log("secondTest");
+console.dir("secondTest");
